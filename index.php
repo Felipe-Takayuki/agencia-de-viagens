1c3e08b2e557332252c34cee81af10c6103f4288
Felipe-Takayuki <felipe.takahashi@etec.sp.gov.br>
2024-02-21 17:37:24 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,38 @@
+
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agência de Viagens</title>
+    <link rel="stylesheet" href="src/style/style.css">
+    <link rel="shortcut icon" href="src/assets/favicon/favicon.ico" type="image/x-icon">
+</head>
+<body>
+  <header>
+
+    <img src="src/assets/LOGO.png" alt="logo" style="width: 90px;">
+    <a href="">Sobre</a>
+    <a href="">Planos de Viagens</a>
+    <a href="">Fale Conosco</a>
+  </header>
+  <article>
+    <section>
+      <p class="welcome">Olá, Seja bem vindo a<br>nossa agência.</p>
+      <p class="local">estamos localizados Av. Castro Alves, 62 - Somenzari, Marília - SP, 17506-000 </p>
+    </section>
+    
+    <section class="viagens">
+      <p class="title">Viage Para</p>
+        <div class="cards">
+           <div class="card"></div>
+           <div class="card"></div>
+           <div class="card"></div>
+           <div class="card"></div>
+        </div>
+    </section>
+
+</body>
+</html>
---
+++ src/pages/pacotes_pagina.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agência de Viagens</title>
+    <link rel="stylesheet" href="../style/style.css">
+    <link rel="stylesheet" href="../style/cards_style.css">
+    <link rel="shortcut icon" href="../assets/favicon/favicon.ico" type="image/x-icon">
+</head>
+<body>
+  <header>
+
+    <img src="../assets/LOGO.png" alt="logo" style="width: 90px;">
+    <a href="/a.html">Sobre</a>
+    <a href="">Planos de Viagens</a>
+    <a href="">Fale Conosco</a>
+  </header>
+
+    <section class="viagens">
+      <p class="title">Viage Para</p>
+        <div class="cards">
+          <a href="">
+            <div class="card">
+              <img src="../assets/imagem dos cards/Gramado.png" alt="Gramado">
+              <p>Gramado</p>
+            </div>
+          </a>
+          <a href="">
+            <div class="card">
+              <img src="../assets/imagem dos cards/Jericoacoara.png" alt="Jericoara">
+              <p>Jericoara</p>
+             </div>
+          </a>
+          <a href="">
+            <div class="card">
+              <img src="../assets/imagem dos cards/Fernando de Noronha.png" alt="Fernando de Noronha">
+              <p>Fernando de Noronha</p>
+             </div>
+          </a>
+          <a href="">
+            <div class="card">
+              <img src="../assets/imagem dos cards/Paranapiacaba.png" alt="Paranapiacaba">
+              <p>Paranapiacaba</p>
+             </div>
+          </a>
+
+        </div>
+    </section>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
