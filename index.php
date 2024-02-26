4950dfa9e35a5ccbfd516dec92857781ea0cabbe
Felipe-Takayuki <felipe.takahashi@etec.sp.gov.br>
2024-02-26 19:53:33 -0300

    promoçoes

--- index.php
+++ index.php
@@ -13,14 +13,28 @@
     <img src="src/assets/LOGO.png" alt="logo" style="width: 90px;">
     <a href="">Sobre</a>
     <a href="src/pages/pacotes_pagina.php">Planos de Viagens</a>
-    <a href="">Fale Conosco</a>
+    <a href="src/pages/fale_conosco.php">Fale Conosco</a>
   </header>
   <article>
     <section>
       <p class="welcome">Olá, Seja bem vindo a<br>nossa agência.</p>
       <p class="local">estamos localizados Av. Castro Alves, 62 - Somenzari, Marília - SP, 17506-000 </p>
     </section>
-    
-
+    <section class="promotions">
+      <div class="promotion-card">
+        <p>Pague em até 12x no Cartão</p>
+      </div>
+      <div class="promotion-card">
+        <p>
+        Hospedagem nos melhores hotéis
+        </p>
+      </div>
+    </section>
+  </article>
+  <footer>
+    <img src="src/assets/LOGO.png">
+    <a href="">Código</a>
+    <p>Site sem fins lucrativos apenas para estudo, feito por Felipe Takahashi</p>
+  </footer>
 </body>
 </html>
